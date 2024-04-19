7e811775081a657c3dc8bfb633d3b98ee249c6aa
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:23:18 +0000

    Adapt to LMODroid by device-add script

--- common.mk
+++ common.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/motorola/sdm632-common/sdm632-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
