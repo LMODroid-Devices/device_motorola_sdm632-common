40d2e0e9eaee0409b0927318963c1282682e7613
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:45:49 +0000

    Adapt to LMODroid by device-add script

--- common.mk
+++ common.mk
@@ -32,7 +32,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
